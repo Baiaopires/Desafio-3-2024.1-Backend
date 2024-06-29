8f270192f2182f35bd9922f7569bb83b5de2fb9a
Vinícius Baião Pires <85589369+Baiaopires@users.noreply.github.com>
2024-06-29 00:09:48 -0300

    Entrega 3 completa

--- src/script.js
+++ src/script.js
@@ -1,23 +1,53 @@
-//função de mudar imagem pelo id e pela url
-function changeImage(id, url) {
-  document.getElementById(id).src = url;
-}
-//função de mudar texto pelo id e pelo texto
-function changeText(id, text) {
-  document.getElementById(id).innerText = text;
+let divNome = document.querySelector("#nome")
+let divImagem = document.querySelector("#imagem")
+let buttonNext = document.getElementById("nextButton");
+let buttonBack = document.getElementById("backButton");
+let counterElement = document.getElementById("counterValue")
+
+// Condições iniciais
+let counter = 1;
+
+// limite de 1025 pokemons
+const url = `https://pokeapi.co/api/v2/pokemon/${counter}`
+
+fetch(url)
+    .then(response => response.json())
+    .then(pokemon => {
+        console.log(pokemon.name)
+        divNome.innerHTML = "<strong>" + pokemon.name + "</strong>";
+    })
+
+function showName(id){
+    const url = `https:pokeapi.co/api/v2/pokemon/${id}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(pokemon => {
+            console.log(pokemon.name)
+            divNome.innerHTML = "<strong>" + pokemon.name + "</strong>";
+            divImagem.innerHTML = "<img src=\"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png\" style=\"text-align: center2\" class=\"center2\"></img>"
+        })
 }
 
-// Daqui para baixo voce ira escrever
-// o código para resolver o desafio
+buttonNext.addEventListener("click", () => {
+    counter++;
+    // limite superior de 1025 pokémons
+    if(counter == 1026){
+        counter = 1;
+    }
+    counterElement.innerHTML = "<strong>#" + counter + "</strong>";
+    
+    showName(counter)
+})
+buttonBack.addEventListener("click", () => {
+    counter--;
 
-function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
-}
+    // limite inferior de 0 pokémons
+    if(counter == 0){
+        counter = 1025;
+    }
 
-function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
-}
+    counterElement.innerHTML = "<strong>#" + counter + "</strong>";
+    
+    showName(counter)
+})
